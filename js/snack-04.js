cb86d248df16d8e204516e28e7a2336f3d0fe800
danieldivincenzo <danieldivincenzo14@gmail.com>
2025-05-16 15:13:35 +0200

    iniziato snack-04

---
+++ js/snack-04.js
@@ -0,0 +1,33 @@
+const persone = [
+    {
+        nome: "Giacomo",
+        cognome: "Rossi",
+        eta: 25
+    },
+
+    {
+        nome: "Martina",
+        cognome: "Bianchi",
+        eta: 16
+    },
+
+    {
+        nome: "Luca",
+        cognome: "Verdi",
+        eta: 31
+    },
+
+    {
+        nome: "Marco",
+        cognome: "Gialli",
+        eta: 18
+    },
+
+    {
+        nome: "Francesco",
+        cognome: "Violi",
+        eta: 17
+    }
+]
+console.log(persone)
+
