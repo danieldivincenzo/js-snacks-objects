22524fd7ea645b29ee51d4bd3e1a4fcdc8ae5b24
danieldivincenzo <danieldivincenzo14@gmail.com>
2025-05-16 13:47:49 +0200

    aggiunta base ciclo for

--- js/snack-01.js
+++ js/snack-01.js
@@ -1,7 +1,74 @@
-const auto = [
+const automobili = [
     {
         marca: "Fiat",
         modello: "Panda",
-        alimentazione: "benzina"
+        alimentazione: "Benzina"
+    },
+
+    {
+        marca: "Ford",
+        modello: "Focus",
+        alimentazione: "Benzina"
+    },
+
+    {
+        marca: "Mercedes",
+        modello: "CLA",
+        alimentazione: "Diesel"
+    },
+
+    {
+        marca: "Jeep",
+        modello: "Avenger",
+        alimentazione: "Elettrico"
+    },
+
+    {
+        marca: "Bmw",
+        modello: "Serie 3",
+        alimentazione: "Diesel"
+    },
+
+    {
+        marca: "Volkswagen",
+        modello: "Polo",
+        alimentazione: "Gpl"
+    },
+
+    {
+        marca: "Alfa Romeo",
+        modello: "Giulietta",
+        alimentazione: "Metano"
+    },
+
+    {
+        marca: "Renault",
+        modello: "Clio",
+        alimentazione: "Benzina"
+    },
+
+    {
+        marca: "Citroen",
+        modello: "C3",
+        alimentazione: "Diesel"
+    },
+
+    {
+        marca: "Peugeot",
+        modello: "308",
+        alimentazione: "Elettrico"
     }
 ]
+console.log(automobili)
+
+let autoBenzina = []
+let autoDiesel = []
+let altreAuto = []
+
+for (let i = 0; i < automobili.length; i++) {
+    const auto = automobili[i];
+    console.log(auto)
+
+
+
+}
