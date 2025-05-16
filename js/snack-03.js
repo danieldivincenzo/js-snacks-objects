93ccc61f5b9cbca251f2cb89df9812e274c87da8
danieldivincenzo <danieldivincenzo14@gmail.com>
2025-05-16 15:02:03 +0200

    iniziata struttura base snack 03, array  di animali

---
+++ js/snack-03.js
@@ -0,0 +1,53 @@
+const animali = [
+    {
+        nome: "leone",
+        famiglia: "felidi",
+        classe: "mammiferi"
+    },
+
+    {
+        nome: "gallina",
+        famiglia: "fasianidi",
+        classe: "uccelli"
+    },
+
+    {
+        nome: "cane",
+        famiglia: "canidi",
+        classe: "mammiferi"
+    },
+
+    {
+        nome: "lucertola",
+        famiglia: "ornitorinchidi",
+        classe: "rettili"
+    },
+
+    {
+        nome: "mantide",
+        famiglia: "mantidi",
+        classe: "insetti"
+    },
+
+    {
+        nome: "gatto",
+        famiglia: "felidi",
+        classe: "mammiferi"
+    },
+
+    {
+        nome: "scorpione",
+        famiglia: "scorpionidi",
+        classe: "aracnidi"
+    },
+
+    {
+        nome: "ornitorinco",
+        famiglia: "ornitorinchidi",
+        classe: "mammiferi"
+    }
+
+]
+console.log(animali)
+let mammiferi = []
+
